996b0abe78b11a25fa10cc6e056c1c1978a22b96
Dmytres Kelven <dmytresksn@hotmail.com>
2019-05-03 11:30:06 -0300

    Custom User Model
    Meio da Aula 47, Custom User Model - Registration model em edição.

--- simplemooc/accounts/forms.py
+++ simplemooc/accounts/forms.py
@@ -1,36 +1,40 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 class RegisterForm(UserCreationForm):
 
-    email = forms.EmailField(label="E-Mail")
+    #email = forms.EmailField(label="E-Mail") #desnecessario pois já está no fields
+    password1 = forms.CharField(label='Senha', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Confirmação de Senha', widget=forms.PasswordInput)
 
-    def clean_email(self):
-        email = self.cleaned_data["email"]
-
-        if User.objects.filter(email=email).exists():
-            raise forms.ValidationError("Esse e-mail já está sendo usado")
-        return email
+    def clean_password2(self):
+        password1 = self.cleaned_data.get("password1")
+        password2 = self.cleaned_data.get("password2")
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError(
+                'A confirmação de senha nao está correta',
+                code='password_mismatch',
+            )
+        return password2
 
     def save(self, commit=True):
         user = super(RegisterForm, self).save(commit=False)
-        user.email = self.cleaned_data['email']
+        #user.email = self.cleaned_data['email']
+        user.set_password(self.cleaned_data['password1'])
                 
         if commit:
             user.save()
         return user
+    
+    class Meta:
+        model = User
+        fields = ['username', 'email']
 
 class EditAccountForm(forms.ModelForm):
 
-    def clean_email(self):
-        email = self.cleaned_data["email"]
-
-        queryset = User.objects.filter(email=email).exclude(pk=self.instance.pk)
-        if queryset.exists():
-            raise forms.ValidationError("Esse e-mail já está sendo usado")
-        return email
-    
     class Meta:
         model = User
         #fields eh uma lista com os campos a serem alterados
